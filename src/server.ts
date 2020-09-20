b2947aa4f74f71347a1e3d4f2a9dea81301bdb18
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2020-09-20 01:35:36 -0400

    🎨 chore: setup prettier

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import "./utils/module-alias";
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { ForecastController } from "./controllers/forecast";
+import './utils/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
