0ef2d5c90b265264284bdf95910e76748cfb4994
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2020-09-23 14:48:35 -0400

    🥅 feat: catching errors for beaches creation

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,12 +1,21 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const beach = await Beach.create(req.body);
-    res.status(201).send(beach.toJSON());
+    try {
+      const beach = await Beach.create(req.body);
+      res.status(201).send(beach.toJSON());
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Error' });
+      }
+    }
   }
 }
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -20,5 +20,28 @@ describe('Beaches functional tests', () => {
       expect(status).toBe(201);
       expect(body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('should return error 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_latitude',
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+      };
+
+      const { body, status } = await global.testRequest
+        .post('/beaches')
+        .send(newBeach);
+
+      expect(status).toBe(422);
+      expect(body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_latitude" at path "lat"',
+      });
+    });
+
+    it.skip('should return error 500 when there is an error other than validation', async () => {
+      // TODO
+    });
   });
 });
