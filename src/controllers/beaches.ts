434e1c2e8c58400251045530da016d88faacff08
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2020-09-23 11:24:58 -0400

    ✨ feat: inserting beaches to db

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,12 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
-  public create(req: Request, res: Response): void {
-    res.status(201).send({ ...req.body, id: 'user-id' });
+  public async create(req: Request, res: Response): Promise<void> {
+    const beach = await Beach.create(req.body);
+    res.status(201).send(beach.toJSON());
   }
 }
