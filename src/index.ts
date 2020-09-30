284d9685980fedb68501704f232c8b9d88ef684f
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2020-09-29 21:42:25 -0400

    🥅 chore: gracefully shutdown and catching unhandled errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
 import config from 'config';
+import logger from './logger';
 import { SetupServer } from './server';
 
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to a unhandled promise ${promise} and reason ${reason}`
+  );
+  throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error(`App exiting due to a uncaughtException: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
+
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('app.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('app.port'));
+    await server.init();
+    server.start();
+
+    // gracefully shutdown
+    const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          await server.close();
+          logger.info('Server gracefully shutdown.');
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
